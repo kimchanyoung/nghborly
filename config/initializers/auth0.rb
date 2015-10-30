3a862e5c7fdd11e56a51c834da25b3c09553827d
saraho-shea <sarahoshea56@gmail.com>
2015-10-30 10:58:09 -0400

    Update auth0 initializer to include client id, client secret, and namespace

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :auth0,
-    'YOUR_CLIENT_ID',
-    'YOUR_CLIENT_SECRET',
-    'YOUR_NAMESPACE',
+    'WuwkvmO3lwnNQO0u7LMQrhYkiKlTjLGW',
+    'a6EklrAqRFEnRPJIY_2IJv120DSIjv9bNnipFkrz6HgAls4v03pnm5JNoVzBpGYC',
+    'nghborly.auth0.com',
     callback_path: "/auth/auth0/callback"
   )
 end
