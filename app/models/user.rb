0de2cc1030f16a57b50ba7d9f56be0260e1a2bd4
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-10-30 10:14:13 -0400

    Add vote associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   belongs_to :group
   has_many :requests, as: :requester
   has_many :requests, as: :responder
+  has_many :votes, as: :voter
+  has_many :votes, as: :candidate
 
   validates :first_name, :last_name, presence: true
   validates :email, presence: true, uniqueness: true
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,7 @@
 class Vote < ActiveRecord::Base
-  validates :voter_id, :candidate_id, presence: true, numericality: { only_integer: true }
+  belongs_to :voter, class_name: 'User'
+  belongs_to :candidate, class_name: 'User'
+
+  validates :voter, :candidate
   validates :value, presence: true, inclusion: { in: [1, -1] }
 end
