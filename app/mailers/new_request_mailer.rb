8840fef26cf724ca4818ea68f1337ff59224ce0e
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-10-30 17:01:31 -0400

    Add f-name var to mail template

--- app/mailers/new_request_mailer.rb
+++ app/mailers/new_request_mailer.rb
@@ -4,6 +4,7 @@ class NewRequestMailer < BaseMandrillMailer
     merge_vars = {
       "REQUEST_URL" => "/requests/#{request.id}",
       "REQUEST_CONTENT" => request.content,
+      "REQUESTER_FNAME" => request.first_name,
       "FIRST_NAME" => user.first_name
     }
     body = mandrill_template('new_request', merge_vars)
