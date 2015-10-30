aeba721b5c306d50755a1ac01033e3ff49dd5413
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-10-30 14:54:35 -0400

    Include vars to be sent over to mail template

--- app/mailers/new_request_mailer.rb
+++ app/mailers/new_request_mailer.rb
@@ -2,7 +2,9 @@ class NewRequestMailer < BaseMandrillMailer
   def notify(request, user)
     subject = "#{request.requester.first_name} needs a neighbor!"
     merge_vars = {
-      ""
+      "REQUEST_URL" => "/requests/#{request.id}",
+      "REQUEST_CONTENT" => request.content,
+      "FIRST_NAME" => user.first_name
     }
     body = mandrill_template('new_request', merge_vars)
 
