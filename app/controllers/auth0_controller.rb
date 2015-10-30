eab0bb89d600075bf81de77b96dd13f637d140f7
saraho-shea <sarahoshea56@gmail.com>
2015-10-30 10:43:19 -0400

    Add the shell for the code that will hand the success and failure of the callback

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -1,5 +1,8 @@
 class Auth0Controller < ApplicationController
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> Add the shell for the code that will hand the success and failure of the callback
 def callback
     # This stores all the user information that came from Auth0
     # and the IdP
@@ -7,6 +10,7 @@ def callback
 
     # Redirect to the URL you want after successfull auth
     redirect_to '/dashboard'
+<<<<<<< HEAD
   end
 
   def failure
@@ -18,5 +22,12 @@ def callback
 
   def failure
 >>>>>>> Add the Auth0 callback handler
+=======
+  end
+
+  def failure
+    # show a failure page or redirect to an error page
+    @error_msg = request.params['message']
+>>>>>>> Add the shell for the code that will hand the success and failure of the callback
   end
 end
