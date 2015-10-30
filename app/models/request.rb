4392ca04753cb6b36336dd82f147b6123d59762a
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-10-30 10:12:57 -0400

    Add user association to requests

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,7 @@
 class Request < ActiveRecord::Base
+  belongs_to :requester, class_name: 'User'
+  belongs_to :responder, class_name: 'User'
+
   validates :content, presence: true
   validates :requester
   validates :isFulfilled, presence: true
