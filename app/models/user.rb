1b9f39d568535bf7f4ab54ee0bc56a3e9780b571
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-10-30 10:03:44 -0400

    Fix user validations to fix auth0

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-  validates :first_name, :last_name, :password_digest, presence: true
+  validates :first_name, :last_name, presence: true
   validates :email, presence: true, uniqueness: true
+  validates :username, :avatar, :uid, presence: true
+  validates :uid, uniqueness: true
   validates :group_id, presence: true, numericality: { only_integer: true }
 end
