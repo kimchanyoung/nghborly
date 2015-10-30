19c47755a6e651b8ba422abf465953a7e933347b
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-10-30 10:11:55 -0400

    Add request associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   belongs_to :group
+  has_many :requests, as: :requester
+  has_many :requests, as: :responder
 
   validates :first_name, :last_name, presence: true
   validates :email, presence: true, uniqueness: true
