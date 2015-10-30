c4f685a56b4bea1c22940f555b4b3775cd40f9ad
Steven Leiva <leiva.steven@gmail.com>
2015-10-30 11:31:19 -0400

    Add route to accept a user's address input.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :requests
-  
+
   get   '/login', :to => 'sessions#new', :as => :login
   get   '/logout', :to => 'sessions#destroy', :as => :logout
   get "/auth/auth0/callback" => "auth0#callback"
   get "/auth/failure" => "auth0#failure"
 
+  post "/groups/assign", to: "groups#assign"
 end
