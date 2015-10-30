4028e067a6f33cef6928eda4d8a61fa4adf194f5
saraho-shea <sarahoshea56@gmail.com>
2015-10-30 12:51:08 -0400

    Update database with userinfo column on users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029212113) do
+ActiveRecord::Schema.define(version: 20151030164851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20151029212113) do
     t.integer  "group_id",        null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "userinfo"
   end
 
   create_table "votes", force: :cascade do |t|
