9f3f7b25e990fc2f67ccbd91497258e4e152c9b3
saraho-shea <sarahoshea56@gmail.com>
2015-10-30 10:38:22 -0400

    Initialize omniauth autho0 and add shell of content

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :auth0,
-    'WuwkvmO3lwnNQO0u7LMQrhYkiKlTjLGW',
-    'a6EklrAqRFEnRPJIY_2IJv120DSIjv9bNnipFkrz6HgAls4v03pnm5JNoVzBpGYC',
+    ENV['AUTH0_CLIENT'],
+    ENV['AUTH0_SECRET'],
     'nghborly.auth0.com',
     callback_path: "/auth/auth0/callback"
-  )
-end
+    )
+end 
