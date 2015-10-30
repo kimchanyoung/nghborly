220d76a93122e54a771f8fd902cd2ef8ec725ab0
Steven Leiva <leiva.steven@gmail.com>
2015-10-30 11:56:18 -0400

    Change groups migration so that the attributes we expect are saved.
    Note that the name is no longer a field on our group table, but it is a
    virtual attribute in our model. We no longer have uniqueness validations
    at the Model level or constraints at the Database level.

--- db/migrate/20151029211234_create_groups.rb
+++ db/migrate/20151029211234_create_groups.rb
@@ -1,8 +1,10 @@
 class CreateGroups < ActiveRecord::Migration
   def change
     create_table :groups do |t|
-      t.string :name, null: false
-      t.string :address, null: false
+      t.integer :number,   null: false
+      t.string  :street,   null: false
+      t.string  :city,     null: false
+      t.integer :zip_code, null: false
 
       t.timestamps null: false
     end
