4977a9b1c4371fbdc0bbc93355c28dc8fbe0314a
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-10-30 10:45:17 -0400

    Add controller for create

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -7,4 +7,21 @@ class RequestsController < ApplicationController
     @request = Request.new
   end
 
+  def create
+    redirect_to new_request and return unless logged_in?
+    @request = Request.new(request_attributes)
+    @request.requester = current_user
+    if @request.save
+
+    else
+
+    end
+  end
+
+  private
+
+  def request_attributes
+    params.require(:request).permit(:content)
+  end
+
 end
