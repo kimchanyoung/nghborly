bf25dc11cc79d004393876005489504ea18c8d62
Steven Leiva <leiva.steven@gmail.com>
2015-10-30 11:53:15 -0400

    Modify Group model so that it has the attributes we expect a user to sign up with.

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,11 @@
 class Group < ActiveRecord::Base
   has_many :users
 
-  validates :name, :address, presence: true, uniqueness: true
+  validates :street, :city, presence: true
+  validates :number, :zip_code,  numericality: { only_integer: true }
+
+  def name
+    "#{number} #{street}"
+  end
+
 end
