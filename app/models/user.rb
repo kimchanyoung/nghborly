3191e55f79e06f436d8a899a43b9437806481455
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-10-30 10:11:21 -0400

    Add group association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
+  belongs_to :group
+
   validates :first_name, :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :username, :avatar, :uid, presence: true
   validates :uid, uniqueness: true
-  validates :group_id, presence: true, numericality: { only_integer: true }
+  validates :group
 end
