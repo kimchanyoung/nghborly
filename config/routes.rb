b5ffc972b6d7c8053fd1f57531f004b8a8632237
saraho-shea <sarahoshea56@gmail.com>
2015-10-30 10:40:12 -0400

    Add the Auth0 callback handler

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :requests  
+
   get   '/login', :to => 'sessions#new', :as => :login
   get   '/logout', :to => 'sessions#destroy', :as => :logout
   get "/auth/auth0/callback" => "auth0#callback"
