5b3fcbc90b6de6b3f228a2db64975323e6c5edcf
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-10-31 16:47:12 -0400

    Render forms

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,14 +1,15 @@
 class RequestEventsController < UserActionsController
   def active
-    Events.where()
+    @transactions = Transaction.sort_by_active
+    render ''
   end
 
   def river
-
+    @transactions = Transaction.sort_by_group(current_user.group)
   end
 
   def history
-
+    @transactions = Transaction.sort_by_user(current_user)
   end
 
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,17 +4,17 @@ class Transaction < ActiveRecord::Base
   validates :transaction_type, inclusion: { in: ['request', 'response', 'fulfillment'] }
 
   def self.sort_by_group(group)
-    Transaction.where(request: Request.joins(:requester).where(users: {group_id: group.id}))
+    @transactions = Transaction.where(request: Request.joins(:requester).where(users: {group_id: group.id}))
+    render 'transactions/index.html.erb'
   end
 
   def self.sort_by_user(user)
-    Transaction.joins(:request).where(requests: {requester_id: user_id})
+    @transactions = Transaction.joins(:request).where(requests: {requester_id: user_id})
+    render 'transactions/index.html.erb'
   end
 
   def self.sort_by_active
-    Transaction.joins(:request).where('transaction_type LIKE :type AND transactions.created_at BETWEEN :then AND :now', type: 'request', then: 1.hour.ago, now: Time.now).where(requests: {is_fulfilled: false})
+    @transactions = Transaction.joins(:request).where('transaction_type LIKE :type AND transactions.created_at BETWEEN :then AND :now', type: 'request', then: 1.hour.ago, now: Time.now).where(requests: {is_fulfilled: false})
+    render 'transactions/index.html.erb'
   end
 end
-
-
-Transaction.joins(:request).where('transaction_type LIKE :type AND transactions.created_at BETWEEN :then AND :now', type: 'request', then: 1.hour.ago, now: Time.now)
