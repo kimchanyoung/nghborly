8d4a6f8fd71cfa310225f7f73f22af1bfdb7e6da
Steven Leiva <leiva.steven@gmail.com>
2015-10-31 15:43:51 -0400

    Add an error message if the user cannot be assigned to a group.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,7 +9,8 @@ class GroupsController < ApplicationController
     if user.save
       redirect_to root_path
     else
-      redirect :back
+      flash[:now] = "We couldn't assign you to a building!"
+      render :inquire
     end
   end
 
