8fcbef8486bcb415184f596c6048ec1b40f8108a
saraho-shea <sarahoshea56@gmail.com>
2015-10-31 10:54:49 -0400

    Add new method for initializing a vote and finding a user

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,4 +1,14 @@
+<<<<<<< HEAD
 class VotesController < UserActionsController
+=======
+class VotesController < ApplicationController
+
+    def new
+        @vote = Vote.new
+        @user = User.find_by(id: params[:user_id])
+    end
+
+>>>>>>> Add new method for initializing a vote and finding a user
     def create
     end
 end
