a91cdf3dcaff0b36b93e4d9937721ba83ea9f884
saraho-shea <sarahoshea56@gmail.com>
2015-10-31 09:38:05 -0400

    Made changes to reflect changes to db

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -1,12 +1,14 @@
 class Auth0Controller < ApplicationController
 def callback
 
-    user = User.find_or_create_by(userinfo: request.env['omniauth.auth']['userinfo'])
+    user = User.find_or_create_by(user_id: request.env['omniauth.auth']['user_id'])
 
-    user.name = request.env['omniauth.auth']['info']['name']
+    user.first_name = request.env['omniauth.auth']['info']['given_name']
+    user.last_name = request.env['omniauth.auth']['info']['family_name']
+    user.email = request.env['omniauth.auth']['info']['email']
     user.save
 
-    session[:userinfo] = user.userinfo
+    session[:user_id] = user.user_id 
 
     redirect_to '/'
   end
