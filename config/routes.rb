b6ed22b7660f1785c83816ff734b6a58b2a6d0ae
Steven Leiva <leiva.steven@gmail.com>
2015-10-31 11:18:32 -0400

    Reformat routes so that resources are at the bottom.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resources :requests
-
   get   '/login', :to => 'sessions#new', :as => :login
   get   '/logout', :to => 'sessions#destroy', :as => :logout
   get "/auth/auth0/callback" => "auth0#callback"
@@ -8,12 +6,9 @@ Rails.application.routes.draw do
 
   post "/groups/assign", to: "groups#assign"
 
-  get '/chatpage', to: 'chats#new'
-  post '/chats/auth', to: 'chats#auth'
-  post '/chats/post', to: 'chats#create'
+  post 'pusher/auth', to: 'pusher#auth'
+
   resources :requests do
     resources :messages
   end
-
-  post 'pusher/auth', to: 'pusher#auth'
 end
