cd389af76fc25fae5de89ce25c9d4a26d6e4588b
saraho-shea <sarahoshea56@gmail.com>
2015-10-31 09:38:40 -0400

    Make changes to database to reflect info we need

--- db/migrate/20151030164851_add_userinfo_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddUserinfoToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :userinfo, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151030165527) do
+ActiveRecord::Schema.define(version: 20151029212113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,16 +35,13 @@ ActiveRecord::Schema.define(version: 20151030165527) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "first_name",      null: false
-    t.string   "last_name",       null: false
-    t.string   "password_digest", null: false
-    t.string   "email",           null: false
-    t.string   "phone"
-    t.integer  "group_id",        null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "userinfo"
-    t.string   "name"
+    t.string   "user_id",    null: false
+    t.string   "first_name", null: false
+    t.string   "last_name",  null: false
+    t.string   "email",      null: false
+    t.integer  "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "votes", force: :cascade do |t|
