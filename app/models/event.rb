658495ad1e092cd3b440ecfad1ccd47a5cf9fff5
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-10-31 16:28:32 -0400

    Replace events with transaction and make sorting class methods

--- app/models/event.rb
+++
@@ -1,5 +0,0 @@
-class Event < ActiveRecord::Base
-  belongs_to :request
-
-  validates :type, inclusion: { in: ['request', 'response', 'fulfillment'] }
-end
---
+++ app/models/transaction.rb
@@ -0,0 +1,20 @@
+class Transaction < ActiveRecord::Base
+  belongs_to :request
+
+  validates :transaction_type, inclusion: { in: ['request', 'response', 'fulfillment'] }
+
+  def self.sort_by_group(group)
+    Transaction.where(request: Request.joins(:requester).where(users: {group_id: group.id}))
+  end
+
+  def self.sort_by_user(user)
+    Transaction.joins(:request).where(requests: {requester_id: user_id})
+  end
+
+  def self.sort_by_active
+    Transaction.joins(:request).where('transaction_type LIKE :type AND transactions.created_at BETWEEN :then AND :now', type: 'request', then: 1.hour.ago, now: Time.now).where(requests: {is_fulfilled: false})
+  end
+end
+
+
+Transaction.joins(:request).where('transaction_type LIKE :type AND transactions.created_at BETWEEN :then AND :now', type: 'request', then: 1.hour.ago, now: Time.now)
