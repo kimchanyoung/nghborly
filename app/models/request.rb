4b2b8e47908cb857880de91f58fa66fa033889fa
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-10-31 12:29:21 -0400

    Change callback to return true

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,12 +1,12 @@
 class Request < ActiveRecord::Base
-  before_save :defaultly_unfulfilled
+  before_create :defaultly_unfulfilled
 
   belongs_to :requester, class_name: 'User'
   belongs_to :responder, class_name: 'User'
 
   validates :content, presence: true
   validates :requester, presence: true
-  validates :is_fulfilled, presence: true
+  validates :is_fulfilled, inclusion: { in: [true, false] }
 
   private
   def defaultly_unfulfilled
