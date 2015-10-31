b2ea2564b808cc5e535324d51085c6b1b731a2ee
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-10-31 16:48:46 -0400

    Remove rendering from models

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,17 +4,14 @@ class Transaction < ActiveRecord::Base
   validates :transaction_type, inclusion: { in: ['request', 'response', 'fulfillment'] }
 
   def self.sort_by_group(group)
-    @transactions = Transaction.where(request: Request.joins(:requester).where(users: {group_id: group.id}))
-    render 'transactions/index.html.erb'
+    Transaction.where(request: Request.joins(:requester).where(users: {group_id: group.id}))
   end
 
   def self.sort_by_user(user)
-    @transactions = Transaction.joins(:request).where(requests: {requester_id: user_id})
-    render 'transactions/index.html.erb'
+    Transaction.joins(:request).where(requests: {requester_id: user_id})
   end
 
   def self.sort_by_active
-    @transactions = Transaction.joins(:request).where('transaction_type LIKE :type AND transactions.created_at BETWEEN :then AND :now', type: 'request', then: 1.hour.ago, now: Time.now).where(requests: {is_fulfilled: false})
-    render 'transactions/index.html.erb'
+    Transaction.joins(:request).where('transaction_type LIKE :type AND transactions.created_at BETWEEN :then AND :now', type: 'request', then: 1.hour.ago, now: Time.now).where(requests: {is_fulfilled: false})
   end
 end
