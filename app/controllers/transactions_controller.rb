5f3e7ef6b39f0b5ab85acf8283c0d5e67da8266e
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-10-31 16:48:36 -0400

    Add logic to render

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,15 +1,17 @@
 class RequestEventsController < UserActionsController
   def active
     @transactions = Transaction.sort_by_active
-    render ''
+    render 'transactions/index.html.erb'
   end
 
   def river
     @transactions = Transaction.sort_by_group(current_user.group)
+    render 'transactions/index.html.erb'
   end
 
   def history
     @transactions = Transaction.sort_by_user(current_user)
+    render 'transactions/index.html.erb'
   end
 
 end
