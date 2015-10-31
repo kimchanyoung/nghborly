c18c471e7c50e94dc339d30c9b5b262c31c1ac18
saraho-shea <sarahoshea56@gmail.com>
2015-10-31 15:17:51 -0400

    Update routes to correspond to user vote

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get '/login', :to => 'sessions#new', :as => :login
   get '/logout', :to => 'sessions#destroy', :as => :logout
+
   get "/auth/auth0/callback" => "auth0#callback"
   get "/auth/failure" => "auth0#failure"
 
