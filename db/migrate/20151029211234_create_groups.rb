b53438e248e8be8ec440d920ca98786ad5d912fe
Steven Leiva <leiva.steven@gmail.com>
2015-10-31 10:20:45 -0400

    Convert type of number and zip_code fields to string from integer.

--- db/migrate/20151029211234_create_groups.rb
+++ db/migrate/20151029211234_create_groups.rb
@@ -1,10 +1,10 @@
 class CreateGroups < ActiveRecord::Migration
   def change
     create_table :groups do |t|
-      t.integer :number,   null: false
+      t.string :number,   null: false
       t.string  :street,   null: false
       t.string  :city,     null: false
-      t.integer :zip_code, null: false
+      t.string :zip_code, null: false
 
       t.timestamps null: false
     end
