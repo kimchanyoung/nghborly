330bebfcd5fe0845eb594ad9c0092bf93efd833d
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-10-31 10:58:17 -0400

    Change default ActionMailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,8 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -38,4 +38,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.perform_deliveries = true
 end
