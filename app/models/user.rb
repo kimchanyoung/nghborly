d6840c776f3b01205e1b9119c1b68f4f6e0f8d91
saraho-shea <sarahoshea56@gmail.com>
2015-10-31 15:13:08 -0400

    Update user model to include method for counting votes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,20 @@
 class User < ActiveRecord::Base
-  belongs_to :group
-  has_many :made_requests, class_name: 'Request', foreign_key: 'requester_id'
-  has_many :fulfilled_requests, class_name: 'Request', foreign_key: 'responder_id'
-  has_many :votes, as: :voter
-  has_many :votes, as: :candidate
+      belongs_to :group
+      has_many :made_requests, class_name: 'Request', foreign_key: 'requester_id'
+      has_many :fulfilled_requests, class_name: 'Request', foreign_key: 'responder_id'
+      has_many :votes, as: :voter
+      has_many :votes, as: :candidate
 
-  validates :first_name, :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
+      validates :first_name, :last_name, presence: true
+      validates :email, presence: true, uniqueness: true
 
-  def requests_in_last_24_hours
-    conditions = { requester_id: id, created_at: (Time.now - 1.day..Time.now) }
-    Request.where(conditions).count
-  end
-end
+      def requests_in_last_24_hours
+        conditions = { requester_id: id, created_at: (Time.now - 1.day..Time.now) }
+        Request.where(conditions).count
+      end
+
+      def count_votes
+        total = self.votes.sum(:value)
+        self.update_attributes(:vote_count, total)
+      end
+    end
