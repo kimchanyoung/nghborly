9c9f58814138cb1fd8529c119f8fa7c65b939d77
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-11-01 17:48:19 -0500

    Limit river results to 25

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,16 +1,16 @@
 class TransactionsController < UserActionsController
   def active
-    @transactions = Transaction.sort_by_active
+    @transactions = Transaction.sort_by_active.limit(25)
     render 'transactions/index.html.erb'
   end
 
   def river
-    @transactions = Transaction.sort_by_group(current_user.group)
+    @transactions = Transaction.sort_by_group(current_user.group).limit(25)
     render 'transactions/index.html.erb'
   end
 
   def history
-    @transactions = Transaction.sort_by_user(current_user)
+    @transactions = Transaction.sort_by_user(current_user).limit(25)
     render 'transactions/index.html.erb'
   end
 
