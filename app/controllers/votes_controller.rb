5c074aa88846138063f4c25ac67b1b3ea60aca1f
saraho-shea <sarahoshea56@gmail.com>
2015-11-01 18:21:53 -0500

    Update logic in votes controller to include request_id

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,7 +2,7 @@ class VotesController < ApplicationController
 
     def create
         @vote = Vote.new(vote_params)
-        @vote.voter_id = current_user.id
+        @vote.candidate_id = other_party_of(Request.find(request_id))
         if vote.save
             flash[:success] = "Thank you for your feedback!"
             redirect_to user_path(current_user)
@@ -11,14 +11,22 @@ class VotesController < ApplicationController
         end
     end
 
+    def other_party_of(request)
+    if current_user == request.requester
+      request.responder
+    elsif current_user == request.responder
+      request.requester
+    end
+end
+
     private 
 
     def vote_params
-        params.require(:vote).permit(:value)
+        params.require(:vote).permit(:value, :request_id)
     end 
 
     def find_candidate
-        @vote.candidate_id = Vote.find(params[:vote][:candidate_id])
+        @vote.candidate_id = Vote.find_by(params[:vote][:candidate_id])
     end 
 end
 
