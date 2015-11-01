abe43e1cf22543001cc7fe63da87340aec72b7cf
saraho-shea <sarahoshea56@gmail.com>
2015-11-01 12:47:03 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
-
   
   get   '/login', :to => 'sessions#new', :as => :login
   get   '/logout', :to => 'sessions#destroy', :as => :logout
