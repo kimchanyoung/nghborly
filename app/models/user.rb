ffd63fd5fbe5e91ff404edfb21e37774253ae7db
saraho-shea <sarahoshea56@gmail.com>
2015-11-01 18:24:10 -0500

    Remove has_many to reflect changes to votes table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
       belongs_to :group
       has_many :made_requests, class_name: 'Request', foreign_key: 'requester_id'
       has_many :fulfilled_requests, class_name: 'Request', foreign_key: 'responder_id'
-      has_many :votes, as: :voter
       has_many :votes, as: :candidate
 
       validates :first_name, :last_name, presence: true
