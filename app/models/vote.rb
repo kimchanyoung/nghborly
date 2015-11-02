bc2778b25db84ec4f3e17879cb439157b19d6b75
saraho-shea <sarahoshea56@gmail.com>
2015-11-02 09:35:12 -0500

    Remove voter

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,15 +1,13 @@
 class Vote < ActiveRecord::Base
     after_save :update_parent_vote_total 
     
-    belongs_to :voter, class_name: 'User'
     belongs_to :candidate, class_name: 'User'
 
-  validates :voter, :candidate, presence: true
+  validates :candidate, presence: true
   validates :value, presence: true, inclusion: { in: [1, -1] }
   validates :candidate_id, uniqueness: {scope: :request_id}
 
   def update_parent_vote_total
         User.find(self.request_id).count_votes
     end
-end
 end
