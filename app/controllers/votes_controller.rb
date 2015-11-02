944fce3b899cc00f29ec480dd3cf55b3b78f16c8
Steven Leiva <leiva.steven@gmail.com>
2015-11-02 16:31:44 -0500

    Refactor Votes Controller.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,30 +1,33 @@
 class VotesController < ApplicationController
-
-    def create
-        @vote = Vote.new(vote_params)
-        @vote.candidate_id = other_party_of(Request.find(request_id))
-        if vote.save
-            flash[:success] = "Thank you for your feedback!"
-            redirect_to user_path(current_user)
-        else
-            flash[:now] = "Your vote has failed"
-            render 'requests/show.html.erb'
-        end
+  def create
+    @vote = Vote.new(vote_params)
+    if @vote.save
+      @vote.candidate.update_vote_total
+      flash[:success] = "Thank you for your feedback!"
+      redirect_to root_path
+    else
+      flash[:now] = "Your vote has failed"
+      render 'requests/show.html.erb'
     end
 
-    def other_party_of(request)
+  end
+
+  def other_party_of(request)
     if current_user == request.requester
       request.responder
     elsif current_user == request.responder
       request.requester
     end
+  end
+
+  private
+
+  def vote_params
+    @request = Request.find_by(id: params[:request_id])
+    @candidate = other_party_of(@request)
+
+    known_attrs = { request_id: @request.id, candidate_id: @candidate.id }
+
+    params.require(:vote).permit(:value).merge(known_attrs)
+  end
 end
-
-    private 
-
-    def vote_params
-        params.require(:vote).permit(:value, :request_id)
-    end 
-end
-
-
