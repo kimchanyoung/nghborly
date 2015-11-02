23513291926cf90f537adb272ba4ff1021b90664
Steven Leiva <leiva.steven@gmail.com>
2015-11-01 19:32:42 -0500

    Add logic to the Groups Controller to use the Smarty Street API for finding an address.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,7 +4,10 @@ class GroupsController < ApplicationController
 
   def assign
     user = current_user
-    user.group_id = find_group(params[:address])
+
+    group = find_group(params[:address])
+
+    user.group_id = group.id
 
     if user.save
       redirect_to root_path
@@ -17,7 +20,31 @@ class GroupsController < ApplicationController
   private
 
   def find_group(address_hash)
-    Group.find_by(address_hash).id
+    normalized_address_params = normalize_address_params(params[:address])
+    Group.find_or_create_by(normalized_address_params)
   end
 
+  def normalize_address_params(address_hash)
+    base_api_endpoint = URI("https://api.smartystreets.com/street-address")
+
+    secrets = {'auth-id' =>  ENV['SMARTY_STREETS_AUTH_ID'], 'auth-token' => ENV['SMARTY_STREETS_AUTH_TOKEN']}
+
+    query = secrets.merge(address_hash)
+
+    base_api_endpoint.query = URI.encode_www_form(query)
+
+    api_response = Net::HTTP.get_response(base_api_endpoint)
+
+    results = JSON.parse(api_response.body)
+
+    clean_results(results.first["components"])
+  end
+
+  def clean_results(hash)
+    unnecessary_keys = %w(plus4_code delivery_point delivery_point_check_digit)
+
+    unnecessary_keys.each { |key| hash.delete(key) }
+
+    hash
+  end
 end
