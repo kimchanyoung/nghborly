32fa4fa9f8d4a97a41e7c4ab76258d9834b854a3
Steven Leiva <leiva.steven@gmail.com>
2015-11-02 16:33:38 -0500

    Removed update_parent_vote_total after save.
    Note that this is a good method, and should be reimplemented once voting
    is working. (An issue will be noted).

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,11 +1,7 @@
 class Vote < ActiveRecord::Base
-  after_save :update_parent_vote_total
-  belongs_to :candidate, class_name: 'User', foreign_key: 'candidate_id'
+  belongs_to :candidate, class_name: 'User'
+  belongs_to :request
 
   validates :candidate, uniqueness: { scope: :request }
   validates :value, inclusion: { in: [1, -1] }
-
-  def update_parent_vote_total
-    User.find(self.request_id).count_votes
-  end
 end
