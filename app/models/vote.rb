e5324096d77bccf29d51088d1e24f173e484de51
Steven Leiva <leiva.steven@gmail.com>
2015-11-02 12:27:12 -0500

    Change validations on the Vote model.
    Presence true is redundant if another validation is being performed.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,13 +1,11 @@
 class Vote < ActiveRecord::Base
-    after_save :update_parent_vote_total 
-    
-    belongs_to :candidate, class_name: 'User'
+  after_save :update_parent_vote_total
+  belongs_to :candidate, class_name: 'User', foreign_key: 'candidate_id'
 
-  validates :candidate, presence: true
-  validates :value, presence: true, inclusion: { in: [1, -1] }
-  validates :candidate_id, uniqueness: {scope: :request_id}
+  validates :candidate, uniqueness: { scope: :request }
+  validates :value, inclusion: { in: [1, -1] }
 
   def update_parent_vote_total
-        User.find(self.request_id).count_votes
-    end
+    User.find(self.request_id).count_votes
+  end
 end
