cb5392ec837a0dcd9fa1b8563904b7438b912e7f
saraho-shea <sarahoshea56@gmail.com>
2015-11-02 16:51:46 -0500

    Update users table to include picture with default image if the user does not provide a photo

--- db/migrate/20151029211323_create_users.rb
+++ db/migrate/20151029211323_create_users.rb
@@ -7,6 +7,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :email, null: false
       t.integer :group_id
       t.integer :vote_count, null: false, default: 0
+      t.string :picture, default: "http://www.gravatar.com/avatar/?s=60&d=identicon"
 
       t.timestamps null: false
     end
