0808ea0a6a91144a5564b02a40445bb475fa4438
Steven Leiva <leiva.steven@gmail.com>
2015-11-03 16:17:20 -0500

    Add a route for handling Status Code 500.

--- config/routes.rb
+++ config/routes.rb
@@ -28,5 +28,5 @@ Rails.application.routes.draw do
   end
 
   match "/404" => "errors#error404", via: [:get, :post, :patch, :delete]
-
+  match "/500" => "errors#error500", via: [:get, :post, :patch, :delete]
 end
