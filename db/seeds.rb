26c531c0874172817804ec18760d5f4f6bfb7c38
Steven Leiva <leiva.steven@gmail.com>
2015-11-03 14:57:21 -0500

    Add a seeds file that populates the database with a group, users, requests, votes, and messages.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-# Create a single group
-
+# Create one grop
 g = Group.find_or_create_by({primary_number: "48",
                          street_name: "Wall",
                          street_suffix: "St",
@@ -15,7 +6,7 @@ g = Group.find_or_create_by({primary_number: "48",
                          state_abbreviation: "NY",
                          zipcode: "10005"})
 
-# Create 5 Users, all in the same group
+# Create 5 Users
 carol = User.create({first_name: "Carol",
                       user_id: "1",
                       last_name: "Shaw",
@@ -77,7 +68,7 @@ requests = ["I need baking soda.",
             "Forgot to send internet payment. Can someone lend me their wifi password, only need 30 minutes?"]
 
 
-# create 5 request for each user, in 3 different states
+# create 5 request for each user
 users.each do |user|
   other_users = users.select {|other_user| user.id != other_user.id  }
 
@@ -87,9 +78,57 @@ users.each do |user|
                         requester_id: user.id,
                         responder_id: other_users.sample.id,
                         group_id: user.group.id})
-
     r.is_fulfilled = true
     r.save
+  end
+
+  # create a conversation between responder and requester
+  request_obj = Request.last
+  timing = (1..45).to_a
+  saluation = ["Hi.",
+               "Hey.",
+               "Hello.",
+               "Hi there.",
+               "Hey there.",
+               "Hello there.",
+               "Are you there?",
+               "How are you?",
+               "Hi neighbor!"]
+  meeting_place = ["in the lobby",
+                   "across the street",
+                   "next to the vending machines",
+                   "by the elevators",
+                   "at the rooftop",
+                   "in the game room"]
+  party_ids = [request_obj.responder.id, request_obj.requester.id]
+  Message.create({request_id: request_obj.id,
+                  content: "#{saluation.sample} I think I can help you out.",
+                  sender_id: party_ids[0]})
+  Message.create({request_id: request_obj.id,
+                  content: "Thank you that would be great!",
+                  sender_id: party_ids[1]})
+  Message.create({request_id: request_obj.id,
+                  content: "I'd like to meet #{meeting_place.sample} if that's OK.",
+                  sender_id: party_ids[0]})
+  Message.create({request_id: request_obj.id,
+                  content: "That's perfect. I can meet you in #{timing.sample}minutes.",
+                  sender_id: party_ids[1]})
+  Message.create({request_id: request_obj.id,
+                  content: "See you then!",
+                  sender_id: party_ids[0]})
+
+  # create vote
+  binary = [1, -1]
+  candidate = party_ids.sample
+  Vote.create({request_id: request_obj.id,
+               candidate_id: candidate,
+               value: binary.sample})
+
+  if binary.sample > 0
+    other_id = party_ids.select {|id| id != candidate }.first
+    Vote.create({request_id: request_obj.id,
+                candidate_id: other_id,
+                value: binary.sample})
   end
 
 
@@ -101,11 +140,10 @@ users.each do |user|
                     group_id: user.group.id})
   end
 
+  # request has no neighbor and is not fulfilled
   1.times do |_|
     Request.create({content: requests.sample,
                     requester_id: user.id,
                     group_id: user.group.id})
   end
-
-  binding.pry
 end
