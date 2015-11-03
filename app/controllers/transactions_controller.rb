ad19c93582a9571bb5961c0a7252260f42859033
Steven Leiva <leiva.steven@gmail.com>
2015-11-03 10:03:54 -0500

    Add logic to the river and history actions in the controller to avoid rendering an entire page if the request is an xhr request.

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,12 +2,21 @@ class TransactionsController < UserActionsController
 
   def river
     @transactions = Transaction.sort_by_group(current_user.group).limit(25)
-    render 'transactions/index.html.erb'
+
+    if request.xhr?
+      render 'transactions/index.html.erb', layout: false
+    else
+      render 'transactions/index.html.erb'
+    end
   end
 
   def history
     @transactions = Transaction.sort_by_user(current_user).limit(25)
-    render 'transactions/index.html.erb'
+    if request.xhr?
+      render 'transactions/index.html.erb', layout: false
+    else
+      render 'transactions/index.html.erb'
+    end
   end
 
 end
