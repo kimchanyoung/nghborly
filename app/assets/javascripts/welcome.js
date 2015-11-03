660781ff7286630b508db9d0934c6fdb4405e924
Steven Leiva <leiva.steven@gmail.com>
2015-11-03 10:03:26 -0500

    Add logic to append the AJAX response from the server to the current river.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,2 +1,15 @@
 $(document).ready(function() {
+  $('.river-form').on('click', function(event) {
+    event.preventDefault();
+
+    var url = event.currentTarget.action;
+
+    $.ajax({
+      url: url
+    })
+    .done(function(data) {
+      $('#river').html('');
+      $('#river').append(data);
+    });
+  });
 });
