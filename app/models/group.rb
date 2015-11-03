dbb4b8c059c605d8016b624d510f899470e805e8
Steven Leiva <leiva.steven@gmail.com>
2015-11-03 17:13:02 -0500

    Remove the presence: true validation from the Group model.
    SmartyStreets will not always return this.

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,8 +2,8 @@ class Group < ActiveRecord::Base
   has_many :users
   has_many :requests
 
-  validates :primary_number, :street_name, :street_suffix, :city_name,
-            :state_abbreviation, :zipcode, presence: true
+  validates :primary_number, :street_name, :city_name, :state_abbreviation,
+            :zipcode, presence: true
 
   def name
     "#{primary_number} #{street_name} #{street_suffix}"
