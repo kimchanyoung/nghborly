d02bd7b624949d587c2ac38796a387d4646c37ea
Steven Leiva <leiva.steven@gmail.com>
2015-11-03 16:17:38 -0500

    Add logic to our customer dispatcher to catch 500 errors in addition to 400 errors.

--- lib/custom_public_exceptions.rb
+++ lib/custom_public_exceptions.rb
@@ -1,7 +1,8 @@
 class CustomPublicExceptions < ActionDispatch::PublicExceptions
   def call(env)
     status = env["PATH_INFO"][1..-1]
-    if status == "404"
+
+    if status == "404" || status == "500"
       Rails.application.routes.call(env)
     else
       super
