ac723f031397ab9a934dcdba49cdca264a479fb3
Steven Leiva <leiva.steven@gmail.com>
2015-11-03 08:05:23 -0500

    Add authentication of subscription to a channel in the Pusher Controller.
    This implements both presence channel authorization (used for the river)
    as well as private channel authorization (used for private messages).

--- app/controllers/pusher_controller.rb
+++ app/controllers/pusher_controller.rb
@@ -1,11 +1,31 @@
 class PusherController < ApplicationController
   def groupauth
-    response = Pusher[params[:channel_name]].authenticate(params[:socket_id], {user_id: current_user.id})
-    render json: response
+    if authorize_presence_channel
+      response = Pusher[params[:channel_name]].authenticate(params[:socket_id], {user_id: current_user.id})
+      render json: response
+    else
+      render :text => "Forbidden", :status => '403'
+    end
   end
 
   def auth
-    response = Pusher[params[:channel_name]].authenticate(params[:socket_id])
-    render json: response
+    if authorize_private_channel
+      response = Pusher[params[:channel_name]].authenticate(params[:socket_id])
+      render json: response
+    else
+      render :text => "Forbidden", :status => '403'
+    end
+  end
+
+  private
+
+  def authorize_presence_channel
+    channel_postfix = params[:channel_name].sub('presence-', '')
+    current_user.group.id == channel_postfix.to_i
+  end
+
+  def authorize_private_channel
+    request = Request.find_by(id: params[:request_id])
+    request.is_party_to?(current_user)
   end
 end
