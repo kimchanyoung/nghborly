fe6235eb2878dd44e4b5525b8da93abed05f696c
Steven Leiva <leiva.steven@gmail.com>
2015-11-04 12:11:29 -0500

    Add Request Model test suite.
    This suite is not yet complete. We are currently showing 90% test
    coverage. Shooting for 100%.

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -17,4 +17,67 @@ describe Request do
     it { should belong_to :responder }
     it { should belong_to :group }
   end
+
+  context "#active?" do
+    context "has no responder" do
+      it "should be active if created right now" do
+        expect(FactoryGirl.create(:request).active?).to be true
+      end
+
+      it "should not be active if created more than 45 minutes ago" do
+        expect(FactoryGirl.create(:request, created_at: 1.hour.ago).active?).to be false
+      end
+    end
+
+    context "has a responder" do
+      it "should be active if it is not fulfilled" do
+        expect(FactoryGirl.create(:request_with_neighbor).active?).to be true
+      end
+
+      it "should not be active it is fulfilled" do
+        # NOTE: trait feature in FactoryGirl not working for boolean.
+        request = FactoryGirl.create(:request_with_neighbor)
+        request.is_fulfilled = true
+        request.save
+
+        expect(request.active?).to be false
+      end
+    end
+  end
+
+  context "#is_party_to?" do
+    let(:request) { FactoryGirl.create(:request_with_neighbor) }
+    let(:user)    { FactoryGirl.create(:user)}
+
+    it "should return false when the argument is not a party to the request" do
+      expect(request.is_party_to?(user)).to be false
+    end
+
+    it "should return true when the argument is a party to the request" do
+      request = FactoryGirl.create(:request_with_neighbor, requester_id: user.id)
+      expect(request.is_party_to?(user)).to be true
+    end
+  end
+
+  context "#can_view?" do
+    let(:user)   { FactoryGirl.create(:user) }
+
+    context "request has no neighbor" do
+      let(:request) { FactoryGirl.create(:request) }
+
+      it "should return false if the user is not part of the group" do
+        expect(request.can_view?(user)).to be false
+      end
+
+      it "should return true if the user is part of the group" do
+        user
+        request = FactoryGirl.create(:request, group_id: user.group.id)
+        expect(request.can_view?(user)).to be true
+      end
+    end
+
+    context "request has a neighbor" do
+      let(:request) { FactoryGirl.create(:request_with_neighbor) }
+    end
+  end
 end
