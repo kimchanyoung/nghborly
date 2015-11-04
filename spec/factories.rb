1f33cfc1dc594d5e0211d02ed248c2b64f0cf2b2
Steven Leiva <leiva.steven@gmail.com>
2015-11-04 08:41:45 -0500

    Initial factory file.
    Note that this factory file only defines factories for the Group, User,
    and Request resources, and excludes the Vote and Transaction resources.
    This was done due to the complexity of these associations. These
    factories will be included in alter releases.
    
    Also note that we can create requests in different states by calling
    request_with_neighbor and fulfilled_request.

---
+++ spec/factories.rb
@@ -0,0 +1,34 @@
+FactoryGirl.define do
+  factory :group do
+    primary_number     { Faker::Number.number(3) }
+    street_name        { Faker::Address.street_name }
+    city_name          { Faker::Address.city }
+    state_abbreviation { Faker::Address.state_abbr }
+    zipcode            { Faker::Address.zip_code }
+  end
+
+  factory :user, aliases: [:requester, :responder] do
+    user_id    { Faker::Number.number(15)}
+    first_name { Faker::Name.first_name }
+    last_name  { Faker::Name.last_name }
+    email      { Faker::Internet.email }
+    association :group
+  end
+
+  factory :request do
+    content { Faker::Lorem.paragraph }
+    association :requester
+    group_id requester.id
+
+    trait :has_neighbor do
+      association :responder
+    end
+
+    trait :is_fulfilled do
+      is_fulfilled true
+    end
+
+    factory :request_with_neighbor, traits: [:has_neighbor]
+    factory :fulfilled_request , traits: [:has_neighbor, :is_fulfilled]
+  end
+end
