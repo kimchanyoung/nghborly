5d0b32d2f7d2efa3e52cbeb2763d1320bfebeda8
Steven Leiva <leiva.steven@gmail.com>
2015-11-04 11:03:12 -0500

    Add test for the User#update_vote_total method. 100% test coverage for the user model. All tests passing.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe User do
+  let(:user) { FactoryGirl.create :user }
+
   context "validations" do
     it { should validate_presence_of :first_name }
     it { should validate_presence_of :last_name }
@@ -15,8 +17,6 @@ describe User do
   end
 
   context "#requests_in_last_24_hours" do
-    let(:user) { FactoryGirl.create :user }
-
     it "should return the number of requests made in the last 24 hours" do
       user
 
@@ -37,6 +37,30 @@ describe User do
       FactoryGirl.create(:request, requester_id: user.id, created_at: 2.days.ago)
 
       expect(user.requests_in_last_24_hours).to eq(2)
+    end
+  end
+
+  context "update_vote_total" do
+    context "positive vote" do
+      it "should increment vote_count by one" do
+        user
+
+        expect{
+          FactoryGirl.create(:vote, candidate_id: user.id, value: 1)
+          user.update_vote_total
+        }.to change{user.vote_count}.by(1)
+      end
+    end
+
+    context "negative vote" do
+      it "should decrement vote_count by one" do
+        user
+
+        expect{
+          FactoryGirl.create(:vote, candidate_id: user.id, value: -1)
+          user.update_vote_total
+        }.to change{user.vote_count}.by(-1)
+      end
     end
   end
 end
