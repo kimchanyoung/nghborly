66da7cb31748a6f10e9c9803fb403ba9a4e8d147
Steven Leiva <leiva.steven@gmail.com>
2015-11-04 10:49:47 -0500

    Added tests for the User#requests_in_last_24_hours.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,30 @@ describe User do
     it { should have_many :fulfilled_requests }
     it { should have_many :votes }
   end
+
+  context "#requests_in_last_24_hours" do
+    let(:user) { FactoryGirl.create :user }
+
+    it "should return the number of requests made in the last 24 hours" do
+      user
+
+      2.times do |_|
+        FactoryGirl.create(:request, requester_id: user.id)
+      end
+
+      expect(user.requests_in_last_24_hours).to eq(2)
+    end
+
+    it "should not count requests made more than 24 hours in the past" do
+      user
+
+      2.times do |_|
+        FactoryGirl.create(:request, requester_id: user.id)
+      end
+
+      FactoryGirl.create(:request, requester_id: user.id, created_at: 2.days.ago)
+
+      expect(user.requests_in_last_24_hours).to eq(2)
+    end
+  end
 end
